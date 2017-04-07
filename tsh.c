62c4694acdbd2ea2f0f2129a394a93e7b76f7261
Jean-Lou <jeanlou601@hotmail.com>
2017-04-07 12:07:44 -0400

    Separation du input en plusieurs argument.

--- tsh.c
+++ tsh.c
@@ -6,18 +6,52 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-int main(void){
+void tokenize(char *string, char *newArgv[], int nbToken){
+    int index = 0;
+    char *token;
+    char *exec;
+    for(int i = 0; i < nbToken; ++i){
+        tokens[i] = calloc(20, sizeof(char));
+    }
+    token = strtok(string, " ");
+    tokens[index] = token;
+    ++index;
+    printf("Token: %s\n", token);
+    while(token != NULL){
+        token = strtok(NULL, " ");
+        tokens[index] = token;
+        printf("Token: %s\n", token);
+        ++index;
+    }
+    for(int i = 0; i <= nbToken; ++i){
+        printf("%s\n", tokens[i]);
+    }
+    
+}
+
+int main(int argc, char *argv[]){
     pid_t id;
+    int nbToken;
     int statut;
+    char *temp;
     char input[56];
     char path[56];
-    char *newArgv[] = {NULL};
+    char *newArgv[];
     char *envp[] = {NULL};
-    
+
     while(1){
         printf("tsh> ");
         char *exe = fgets(input, sizeof(input), stdin);
+        strcpy(temp, exe);
+        for(nbToken = 0; temp[nbToken]; temp[nbToken]==' ' ? ++nbToken : *temp++);
         
+        char *tokens[nbToken + 1];
+        printf("Nb de token: %d\n", nbToken);
+        printf("test1\n");
+    
+        tokenize(exe, tokens, nbToken);
+
+
         if(exe != NULL){
             exe[strlen(input) - 1] = '\0';
             if(!strcmp(input, "exit")) exit(0);
