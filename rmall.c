06addf040d1147ba2a760c439ceaad8ebb4c67b0
Etienne <etiennebergeron91@gmail.com>
2017-04-11 21:50:14 -0400

    Remplacer nftw par fts

--- rmall.c
+++ rmall.c
@@ -1,35 +1,41 @@
-#define _XOPEN_SOURCE 500
-#include <ftw.h>
+#include <fts.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int verifierPermissions(const char *nom);
-
 int main(int argc, char *argv[]){
-    char *nom = argv[1];
+    FTS *contenu;
+    FTSENT *fichier;
+    char *nom[2] = {argv[1], NULL};
 
     if(argc != 2){
         fprintf(stderr, "Nom de repertoire manquant ou arguments en trop\n");
         return EXIT_FAILURE;
     }
 
-    if(nftw(nom, verifierPermissions, 1, 0) == -1){
-        fprintf(stderr, "Permissions insuffisantes\n");
-        return EXIT_FAILURE;
-    }
-
-    if(nftw(nom, remove, 1, FTW_DEPTH) == -1){
+    if(access(nom[0], F_OK) == -1){
         fprintf(stderr, "Répertoire introuvable\n");
         return EXIT_FAILURE;
     }
 
+    contenu = fts_open(nom, FTS_NOCHDIR, 0);
+    while((fichier = fts_read(contenu))){
+        if(access(fichier->fts_path, W_OK) == -1){
+            fprintf(stderr, "Permissions insuffisantes\n");
+            fts_close(contenu);
+            return EXIT_FAILURE;
+        }
+    }
+    fts_close(contenu);
+
+    contenu = fts_open(nom, FTS_NOCHDIR, 0);
+    while((fichier = fts_read(contenu))){
+        if(fichier->fts_info != FTS_D)
+            remove(fichier->fts_path);
+    }
+    fts_close(contenu);
+
     printf("Répertoire supprimé\n");
 
     return EXIT_SUCCESS;
-}
-
-int verifierPermissions(const char *nom){
-
-    return access(nom, W_OK);
 }
--- size.c
+++ size.c
@@ -2,40 +2,38 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <ftw.h>
-
-int nbFichiers = 0;
-int taille = 0;
-
-int calculerTaille(const char *nom);
+#include <fts.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]){
-    char *nom = argv[1];
+    FTS *contenu;
+    FTSENT *fichier;
+    struct stat buf;
+    char *nom[2] = {argv[1], NULL};
+    int nbFichiers = 0;
+    int taille = 0;
 
     if(argc != 2){
         fprintf(stderr, "Nom de repertoire manquant ou arguments en trop\n");
         return EXIT_FAILURE;
     }
 
-    if(ftw(nom, calculerTaille, 1) == -1){
+    if(access(nom[0], F_OK) == -1){
         fprintf(stderr, "Répertoire introuvable\n");
         return EXIT_FAILURE;
     }
 
-    printf("Le répertoire %s contient %d fichiers pour un total de %d octets.\n", nom, nbFichiers, taille);
+    contenu = fts_open(nom, FTS_NOCHDIR, 0);
+    while((fichier = fts_read(contenu))){
+        stat(fichier->fts_path, &buf);
+        if(!S_ISDIR(buf.st_mode)){
+            nbFichiers++;
+            taille += buf.st_size;
+        }
+    }
+    fts_close(contenu);
+
+    printf("Le répertoire %s contient %d fichiers pour un total de %d octets.\n", nom[0], nbFichiers, taille);
 
     return EXIT_SUCCESS;
-}
-
-int calculerTaille(const char *nom){
-    struct stat buf;
-
-    stat(nom, &buf);
-
-    if(!S_ISDIR(buf.st_mode)){
-        nbFichiers++;
-        taille += buf.st_size;
-    }
-
-    return 0;
 }
